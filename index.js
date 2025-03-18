26c14cf8cd12398da964979431ae844bed2bdacb
jedidiah <jedaqsaul@gmail.com>
2025-03-18 15:07:00 +0300

    completed the js functions continued code grade

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun("play football"));
+
+function mondayWork(argument = "go to the office") {
+  return `This Monday, I will ${argument}.`;
+}
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+console.log(wrapAdjective("%")("a dedicated programmer"));
